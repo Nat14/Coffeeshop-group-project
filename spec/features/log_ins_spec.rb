4c3ddacad576e007e791e1ad6daa13b5d626d410
donohue76 <donohue76@hotmail.com>
2016-03-22 13:33:11 -0700

    added testing for registration page

--- spec/features/log_ins_spec.rb
+++ spec/features/log_ins_spec.rb
@@ -6,6 +6,7 @@ RSpec.feature "LogIns", type: :feature do
     visit "/users/sign_in"
       expect(page).to have_content("Log in")
     end
+
     it "will allow a user to enter an email address, password, and login" do
       visit "/users/sign_in"
       fill_in 'Email', with: 'J@yahoo.com'
@@ -13,5 +14,47 @@ RSpec.feature "LogIns", type: :feature do
       click_button 'Log in'
     end
   end
+
+  describe "as a user, I can visit the registration page" do
+    it "will allow a user to visit the registration page" do
+    visit "/users/sign_up"
+      expect(page).to have_content("Sign up")
+    end
+
+    it "will allow a user to enter an email address, password, password confirm and register" do
+      visit "/users/sign_up"
+      fill_in 'Email', with: 'J@yahoo.com'
+      fill_in 'Password', with: 'password123'
+      fill_in 'Password confirmation', with: 'password123'
+      click_button 'Sign up'
+      expect(page).to have_content("Welcome! You have signed up successfully.")
+    end
+
+    it "requires an email address" do
+      visit "/users/sign_up"
+      fill_in 'Password', with: 'password123'
+      fill_in 'Password confirmation', with: 'password123'
+      click_button 'Sign up'
+      expect(page).to have_content("Email can't be blank")
+    end
+
+    it "requires a password" do
+      visit "/users/sign_up"
+      fill_in 'Email', with: 'J@yahoo.com'
+      fill_in 'Password confirmation', with: 'password123'
+      click_button 'Sign up'
+      expect(page).to have_content("Password can't be blank")
+    end
+
+    it "requires password and password confirmation to be the same" do
+      visit "/users/sign_up"
+      fill_in 'Email', with: 'J@yahoo.com'
+      fill_in 'Password', with: 'password123'
+      click_button 'Sign up'
+      expect(page).to have_content("Password confirmation doesn't match Password")
+    end
+  end
+
+
 
 end
