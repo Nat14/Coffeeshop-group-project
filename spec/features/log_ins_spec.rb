ee16ed028ca183da463e35d84689481ad793ff85
donohue76 <donohue76@hotmail.com>
2016-03-22 11:50:32 -0700

    added feature test for devise login

--- spec/features/log_ins_spec.rb
+++ spec/features/log_ins_spec.rb
@@ -1,8 +1,17 @@
 require 'rails_helper'
 
 RSpec.feature "LogIns", type: :feature do
-  it "as a user, I can visit the login page" do
+  describe "as a user, I can visit the login page" do
+    it "will allow a user to visit the login page" do
     visit "/users/sign_in"
       expect(page).to have_content("Log in")
+    end
+    it "will allow a user to enter an email address, password, and login" do
+      visit "/users/sign_in"
+      fill_in 'Email', with: 'J@yahoo.com'
+      fill_in 'Password', with: 'password123'
+      click_button 'Log in'
+    end
   end
+
 end
