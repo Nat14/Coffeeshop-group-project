95700aeb074bd66450da616031a7b3dedd6251a5
jkuey24 <jkuey24@gmail.com>
2016-03-22 17:03:37 -0700

    updated clock

--- app/assets/javascripts/clock.js
+++ app/assets/javascripts/clock.js
@@ -1,27 +1,80 @@
-$(document).ready(function updateClock () {
+var oClockAnalog = {
+    aSecond:         [],
+    dtDate:          new Date(),
+    iCurrSecond:     -1,
+    iHourRotation:   -1,
+    iMinuteRotation: -1,
+    iStepSize:       10,
+    iTimerAnimate:   setInterval("oClockAnalog.fAnimate()", 20),
+    iTimerUpdate:    setInterval("oClockAnalog.fUpdate()", 1000),
 
+    fAnimate:       function() {
+        if (this.aSecond.length > 0) {
+            this.fRotate("analogsecond", this.aSecond[0]);
+            this.aSecond = this.aSecond.slice(1);
+        }
+    },
+    fGetHour:     function() {
+        var iHours = this.dtDate.getHours();
+        if (iHours > 11) {
+            iHours -= 12;
+        }
+        return Math.round((this.dtDate.getHours() * 30) + (this.dtDate.getMinutes() / 2) + (this.dtDate.getSeconds() / 120));
+    },
+    fGetMinute:     function() {
+        return Math.round((this.dtDate.getMinutes() * 6) + (this.dtDate.getSeconds() / 10));
+    },
+    fInit:          function() {
+        this.iHourRotation = this.fGetHour();
+        this.fRotate("analoghour", this.iHourRotation);
 
-var currentTime = new Date();
-var currentHours = currentTime.getHours();
-var currentMinutes = currentTime.getMinutes();
-var currentSeconds = currentTime.getSeconds();
+        this.iMinuteRotation = this.fGetMinute();
+        this.fRotate("analogminute", this.iMinuteRotation);
 
-//add zeros to minutes and seconds, when below 10.
-currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes
-currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds
+        this.iCurrSecond = this.dtDate.getSeconds();
+        this.fRotate("analogsecond", (6 * this.iCurrSecond));
+    },
+    fRotate:        function(sID, iDeg) {
+        var sCSS = ("rotate(" + iDeg + "deg)");
+        $("#" + sID).css({ '-moz-transform': sCSS, '-o-transform': sCSS, '-webkit-transform': sCSS });
+    },
+    fStepSize:     function(iTo, iFrom) {
+        var iAnimDiff = (iFrom - iTo);
+        if (iAnimDiff > 0) {
+            iAnimDiff -= 360;
+        }
+        return iAnimDiff / this.iStepSize;
+    },
+    fUpdate:        function() {
+        // update time
+        this.dtDate = new Date();
 
-//add "AM" or "PM"
-var timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";
+        // hours
+        var iTemp = this.fGetHour();
+        if (this.iHourRotation != iTemp) {
+            this.iHourRotation = iTemp;
+            this.fRotate("analoghour", iTemp);
+        }
 
-//convert to 12 hr clock
-currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
+        // minutes
+        iTemp = this.fGetMinute();
+        if (this.iMinuteRotation != iTemp) {
+            this.iMinuteRotation = iTemp;
+            this.fRotate("analogminute", iTemp);
+        }
 
-//convert hour of "0" to "12"
-currentHours = ( currentHours == 0 ) ? 12 : currentHours;
+        // seconds
+        if (this.iCurrSecond != this.dtDate.getSeconds()) {
+            var iRotateFrom = (6 * this.iCurrSecond);
+            this.iCurrSecond = this.dtDate.getSeconds();
+            var iRotateTo = (6 * this.iCurrSecond);
 
-//full time string
-var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
-
-//update time display
-document.getElementById("clock").firstChild.nodeValue = currentTimeString;
-})
+            // push steps into array
+            var iDiff = this.fStepSize(iRotateTo, iRotateFrom);
+            for (var i = 0; i < this.iStepSize; i++) {
+                iRotateFrom -= iDiff;
+                this.aSecond.push(Math.round(iRotateFrom));
+            }
+        }
+    }
+};   ClockInit();
