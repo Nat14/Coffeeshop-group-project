0623a00609fd2c978604e4381674e4fa8006a406
jkuey24 <jkuey24@gmail.com>
2016-03-23 16:48:08 -0700

    Changing styles

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,13 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  # 
+  # has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200" }
+  # validates_attachment :image, presence: true,
+  #   content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] },
+  #   size: { in: 0..10.megabytes }
+
 end
