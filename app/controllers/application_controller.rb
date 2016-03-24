3a50e5b2de3a147d67c05812e996570bb5beedb0
JonRichardson <jrichy@sbcglobal.net>
2016-03-24 11:09:01 -0700

    corrected confirmation password bug and avatar is added to table through registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password) }
+      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation, :avatar) }
       devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :avatar) }
   end
 
