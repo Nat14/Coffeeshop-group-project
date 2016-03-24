6f4bb84fd3892cb57993e9fa290018e344a2d3fc
JonRichardson <jrichy@sbcglobal.net>
2016-03-24 13:41:28 -0700

    formatting for thumbnail avatart in bootstrap nav bar

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@ class User < ActiveRecord::Base
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
+      
     end
   end
 
