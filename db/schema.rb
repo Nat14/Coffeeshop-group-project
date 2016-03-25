557f118f8d54292a19214ec4174667b75c3aa181
JonRichardson <jrichy@sbcglobal.net>
2016-03-25 16:50:56 -0700

    removed image fields from db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160324201544) do
+ActiveRecord::Schema.define(version: 20160325225213) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,10 +59,6 @@ ActiveRecord::Schema.define(version: 20160324201544) do
     t.datetime "avatar_updated_at"
     t.string   "provider"
     t.string   "uid"
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.integer  "image_file_size"
-    t.datetime "image_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
