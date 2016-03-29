ddea2902326aaaa064f23b03027d79d0417f3b21
JonRichardson <jrichy@sbcglobal.net>
2016-03-29 09:17:50 -0700

    starting model unit test for search

--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe Search, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Search do
+
+  it "must belong to a User" do
+    search =  Search.new(user_search: "ruby")
+    expect(search.save).to be false
+
+    user = User.new
+    user.email = 'J@yahoo.com'
+    user.password = 'password123'
+    user.avatar =  File.new('/Users/learn/Desktop/Coffeeshop-group-project/spec/Images/coffeecup.jpeg')
+    user.save
+    user.searches << Search.new(user_search: "ruby")
+    expect(user.searches.first.user_search).to include("ruby")
+  end
+
+
 end
