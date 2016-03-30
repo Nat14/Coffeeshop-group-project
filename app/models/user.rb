7f67a60a0da389540c01a205cd92fdb9dd99af95
jkuey24 <jkuey24@gmail.com>
2016-03-30 09:20:03 -0700

    Minor formating changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ActiveRecord::Base
-  
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :omniauthable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }
+  has_attached_file :avatar, :styles => { :medium => "400x400>", :thumb => "50x50#" }
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   validates_attachment :avatar, presence: true
 
