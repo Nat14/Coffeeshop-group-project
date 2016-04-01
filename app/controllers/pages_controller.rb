32bbfc5706f33a303f34b9ce26f895b3c64a1a85
kgallivan <kpgallivan@gamil.com>
2016-03-31 17:04:46 -0700

    updated time filter

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,8 +8,11 @@ class PagesController < ApplicationController
 #this is the method that allows us to show user history if they are not logged in
   def profile
 
-    @meetings = Meeting.all
-    @usermeetings = Usermeeting.where(user_id: current_user.id)
+    # @meetings = Meeting.all
+    usermeetings = Usermeeting.where(user_id: params[:email])
+    # TODO: this is for ordering meeting on profile page
+    meetings = usermeetings.map { |usermeeting| usermeeting.meeting }
+    @meetings = meetings.sort_by {|meeting| meeting.time}
     @user = (params[:email])
     # @searches.user_id = current_user.id
     #this gives a list of saved words for current user
