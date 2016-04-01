45ec8266f62dbabfdd58cb846dea970e59323d0b
jkuey24 <jkuey24@gmail.com>
2016-04-01 11:58:21 -0700

    trying to save our work

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160331174934) do
+ActiveRecord::Schema.define(version: 20160330184952) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,26 +63,24 @@ ActiveRecord::Schema.define(version: 20160331174934) do
   add_index "usermeetings", ["user_id"], name: "index_usermeetings_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                                           default: "", null: false
-    t.string   "encrypted_password",                              default: "", null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                                   default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                                                   null: false
-    t.datetime "updated_at",                                                   null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
     t.string   "avatar_file_name"
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
     t.string   "provider"
     t.string   "uid"
-    t.decimal  "latitude",               precision: 10, scale: 6
-    t.decimal  "longitude",              precision: 10, scale: 6
     t.string   "username"
   end
 
