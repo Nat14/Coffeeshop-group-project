6528090058834232e4fdbbb1ac9c1c13b3fcb0a1
jkuey24 <jkuey24@gmail.com>
2016-04-05 11:13:42 -0700

    changed form for fb _login

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :searches
 
   resources :posts, except: [:new, :create]
+  
 
   devise_for :users, controllers: {:registrations => "registrations", omniauth_callbacks: "omniauth_callbacks"}
 
