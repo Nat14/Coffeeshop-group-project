030e269b227432d361cf31a05182862f495c292d
jkuey24 <jkuey24@gmail.com>
2016-04-05 10:03:49 -0700

    added username for fb login

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,35 +17,37 @@ class User < ActiveRecord::Base
 
   has_many :posts
 
-  def self.from_omniauth(auth)
-    if self.where(email: auth.info.email).exists?
-      return_user = self.where(email: auth.info.email).first
-      return_user.provider = auth.provider
-      return_user.uid = auth.uid
-    else
-      return_user = self.create do |user|
-        user.provider = auth.provider
-        user.uid = auth.uid
-        user.username = auth.info.username
-        user.email = auth.info.email
-        user.oauth_token = auth.credentials.token
-        user.oauth_expires_at = Time.at(auth.credentials.expires_at)
-      end
-    end
-
-    return_user
-  end
-
-
   # def self.from_omniauth(auth)
-  #   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-  #     user.provider = auth.provider
-  #     user.uid = auth.uid
-  #     user.email = auth.info.email
-  #     user.avatar =  "https://graph.facebook.com/#{auth["uid"]}/picture?type=large"
-  #
+  #   if self.where(email: auth.info.email).exists?
+  #     return_user = self.where(email: auth.info.email).first
+  #     return_user.provider = auth.provider
+  #     return_user.uid = auth.uid
+  #   else
+  #     return_user = self.create do |user|
+  #       user.provider = auth.provider
+  #       user.uid = auth.uid
+  #       user.username = auth.info.first_name
+  #       user.email = auth.info.email
+  #       user.oauth_token = auth.credentials.token
+  #       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+  #     end
   #   end
+  #
+  #   return_user
   # end
+
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.email = auth.info.email
+      user.username = auth.info.first_name
+
+      user.avatar =  "https://graph.facebook.com/#{auth["uid"]}/picture?type=large"
+
+    end
+  end
 
   def self.new_with_session(params, session)
     if session["devise.user_attributes"]
