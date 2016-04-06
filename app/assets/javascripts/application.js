ebefd986f6ac7a2c9ca07abdac996f0b5d283a83
jkuey24 <jkuey24@gmail.com>
2016-04-06 09:54:29 -0700

    trying to get datepicker to work by adding jquery turbolinks again

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,9 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require jquery-ui/datepicker
-//= require jquery.turbolinks
 //= clock
 //= require bootstrap
 //= require underscore
